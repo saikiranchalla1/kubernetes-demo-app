a46ce5807bbd0a7931a3bd941b6712031d9921e8
schalla <schalla@adobe.com>
2020-06-04 09:58:22 -0600

    first commit

---
+++ Commands.sh
@@ -0,0 +1,8 @@
+docker run -d -p 6033:3306 --name=docker-mysql --env="MYSQL_ROOT_PASSWORD=root" --env="MYSQL_PASSWORD=root" --env="MYSQL_DATABASE=book_manager" mysql
+
+docker exec -it docker-mysql bash;
+
+# Login using `mysql -uroot -p` command and "root" as the password
+docker exec -i docker-mysql mysql -uroot -proot book_manager < sql/book_manager.sql
+
+# https://www.cloudtechnologyexperts.com/kubeadm-on-aws/
